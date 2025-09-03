bfc0c5cfc5f828e42989b51369643a5ddcde0f53
gunner206 <gunner.alx@gmail.com>
2025-09-03 18:50:03 +0100

    basic struct

---
+++ MusicPlaylist.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Song{
+    int id;
+    char title[50];
+    char artist[50];
+    float duration;
+    struct Song *next;
+    struct Sond *prev;
+}
+
+
+
+void main(){
+
+}
